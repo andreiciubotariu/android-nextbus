8391edc0ebb7d37dba37929074a1b275ef9d9d84
Andrei <andrei.development@gmail.com>
2013-09-18 21:41:02 -0400

    A coulple of classes repackaged

---
+++ src/com/andrei/nextbus/library/objects/MultiStopPredictions.java
@@ -0,0 +1,17 @@
+package com.andrei.nextbus.library.objects;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public  class MultiStopPredictions implements MapInitializable {
+	public List <MultiStopDirection> directions = new ArrayList <MultiStopDirection> ();
+	public Map <String,String> attribs;
+
+	public MultiStopPredictions (){
+	}
+	
+	public void init (Map <String,String> attribs){
+			this.attribs = attribs;
+	}
+}
