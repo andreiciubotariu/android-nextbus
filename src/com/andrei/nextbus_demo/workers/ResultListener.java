e9c8f6e556768fb7f14d818992503d9955126683
Andrei Ciubotariu <andrei.development@gmail.com>
2015-06-25 23:10:06 -0400

    Remove old src dir

--- src/com/andrei/nextbus_demo/workers/ResultListener.java
+++
@@ -1,6 +0,0 @@
-package com.andrei.nextbus_demo.workers;
-
-public interface ResultListener {
-
-	public void onResultObtained (Object result);
-}
--- src/com/andrei/nextbus_demo/workers/Startable.java
+++
@@ -1,6 +0,0 @@
-package com.andrei.nextbus_demo.workers;
-
-public interface Startable {
-
-	public void start(boolean forceRefresh);
-}
